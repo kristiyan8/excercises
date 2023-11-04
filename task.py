68c934f82ec0a61a71cf15b7055dd28a2f01d1a9
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2023-11-04 15:19:38 +0200

    2
    2

--- task.py
+++ task.py
@@ -1,1 +1,22 @@
 print("hello")
+{
+    
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "Python: Django",
+            "type": "python",
+            "request": "launch",
+            "program": "${workspaceFolder}\\manage.py",
+            "args": [
+                "runserver"
+            ],
+            "django": true,
+            "justMyCode": true
+        }
+    ]
+}
+print ("hello")
+x=5
+y=6
+print(x+y)
