eec63dafa90bccd491e3dc1cf3f226cbb1e42b16
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-06 20:38:56 +0200

    2342
    424

--- task.js
+++ task.js
@@ -2,9 +2,11 @@
 <html>
     <body>
         <h2>trying something new for me</h2>
-        <button type ="button">
+        <button type ="button"
+        onclick="document.getElementByID('demo').innerHTML = Date()">Click me to display Date and Time
             
         </button>
+        <p id ="demo"></p>
 
 
     </body>
