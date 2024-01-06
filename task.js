8fa5b6d63f8ad63d4ea0b2723cd8c507f9e4a557
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-06 20:44:52 +0200

    3242
    4

--- task.js
+++ task.js
@@ -1,12 +1,12 @@
 
 <html>
     <body>
-        <h2>trying something new for me</h2>
-        <button type ="button"
-        onclick="document.getElementByID('demo').innerHTML = Date()">Click me to display Date and Time
-            
-        </button>
-        <p id ="demo"></p>
+        
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
 
 
     </body>
