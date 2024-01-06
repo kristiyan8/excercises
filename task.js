7dffd8ba3f743f0fbe9fdd6cf8eb9d6751e3aa05
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-06 20:35:00 +0200

    24
    1

--- task.js
+++ task.js
@@ -1,7 +1,11 @@
+
 <html>
     <body>
         <h2>trying something new for me</h2>
-        <p id = "demo">this is a test</p>
+        <button type ="button">
+            
+        </button>
+
 
     </body>
 
