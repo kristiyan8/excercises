52f0922197136c6ecabaec2110f55fa436552ab4
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-08 19:59:06 +0200

    234
    24

--- task.js
+++ task.js
@@ -1,3 +1,10 @@
 
-var msg = 'Hello World';
-console.log(msg);
+<html>
+<body>
+  <h1>Tova e test</h1>
+  <p id="test">test</p>
+  <button type = "button"
+  onclick="document.getElementByid('test').innerHTML = Date()">
+    Click me to display Date and Time.</button>  
+</body>
+</html>
