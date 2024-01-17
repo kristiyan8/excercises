93a1edd14ce83bc4772926c417112e9477c6ed8b
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-17 15:17:49 +0200

    24
    242

---
+++ projectarchitect.js
@@ -0,0 +1,11 @@
+function projectCreation(input) {
+    let architectname = input[0];
+    let projectCount = Number(input[1]);
+
+    let projectHours = projectCount * 3;
+
+    
+
+
+}
+projectCreation(["George" , "4"]);
