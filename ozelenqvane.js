324b05baf5592cefb1eb29b744235fb19f564b36
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-18 21:20:22 +0200

    1313
    1313

---
+++ ozelenqvane.js
@@ -0,0 +1,11 @@
+function priceWork(input) {
+    let price = Number(input["0"]);
+    let area = Number(input["1"]);
+
+    let finalPrice = area * 7.61;
+    
+
+
+
+}
+priceWork(["550" , "2500"])
