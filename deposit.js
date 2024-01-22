6467ca89f10d09002e5e52c27682449f49b52318
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-22 20:45:23 +0200

    234
    242

--- deposit.js
+++ deposit.js
@@ -3,9 +3,11 @@ function calcDeposit(input) {
     let depositPeriod = Number(input[1]);
     let annualPercent = Number(input[2]);
 
-    let sum = depositSum + depositPeriod * ((depositSum * annualPercent) / 12);
+    let annualPercentDecimal = annualPercent / 100;
+
+    let sum = depositSum + depositPeriod * ((depositSum * annualPercentDecimal) / 12);
     console.log(sum);
 
 
 }
-calcDeposit(["200 ", "3 ", "5.7 "]);
+calcDeposit([" 200 ",  "3 ", " 5.7 "]);
