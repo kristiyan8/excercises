d28b2196b72b7bae3e26942dfcdc1885efcc45e8
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-23 21:02:21 +0200

    4
    54

--- 04- vacation.js
+++ 04- vacation.js
@@ -1,16 +1,16 @@
- function vacationBookList(input) {
+function vacationBookList(input) {
     let curBookPages = Number(input[0]);
     let pagesPerHour = Number(input[1]);
     let day = Number(input[2]);
 
     let totalHours = curBookPages / pagesPerHour;
-    let hoursPerDay = totalHours / day ;
-    
+    let hoursPerDay = totalHours / day;
+
     console.log(hoursPerDay);
 
- }
+}
 
- vacationBookList(["212 ",
- "20 ",
- "2 "]);
+vacationBookList(["212 ",
+    "20 ",
+    "2 "]);
 
