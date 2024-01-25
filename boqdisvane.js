c3a5a36325758e2ac5b54c10e4817d8b9a27eb87
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-25 13:18:42 +0200

    243
    24

---
+++ boqdisvane.js
@@ -0,0 +1,19 @@
+function repaiting(input) {
+    let nylonQty = navigator(input[0]);
+    let paintQty = navigator(input[1]);
+    let thinQty = navigator(input[2]);
+    let workingHours = navigator(input[3]);
+
+    
+
+
+
+
+}
+repaiting(["10 ",
+
+    "11 ",
+
+    "4 ",
+
+    "8 "]);
