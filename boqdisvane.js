194da7a97347f2b9a1a7577eeb50a4ddeb084b4f
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-25 13:23:35 +0200

    243
    2423

--- boqdisvane.js
+++ boqdisvane.js
@@ -4,7 +4,19 @@ function repaiting(input) {
     let thinQty = navigator(input[2]);
     let workingHours = navigator(input[3]);
 
+    let extraNailon = 2;
+    let totalNailonQty = nylonQty + 2;
+
+    let nylonPrice = totalNailonQty * 1.50;
+
+    let extraPaintQty = 0.1 * paintQty;
+    let totalPaintQty = paintQty + extraPaintQty;
+    let paitPrice = totalPaintQty * 14.50;
+
+    let thinnerPrice = thinQty * 5;
+
     
+
 
 
 
