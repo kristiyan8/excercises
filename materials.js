baf7508349e7b1ec753059c93c25bb1af17dffd4
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-27 21:18:50 +0200

    242
    42

--- materials.js
+++ materials.js
@@ -1,9 +1,25 @@
- 
- 
- 
- 
- 
+
  function materialsPrice(input) {
+    let pencilCount = Number(input[0]);
+    let markerCount = Number(input[1]);
+    let preparatCount = Number(input[2]);
+    let discountPrice = Number(input[3]);
+   
+
+   let pencilPrice = pencilCount * 5.80;
+   let markerPrice = markerCount * 7.20;
+   let preparatPrice = preparatCount * 1.20;
+   let totalPrice = pencilPrice + markerPrice + preparatPrice;
+   
+
+   let totalDiscount = totalPrice - (totalPrice * 0.25);
+
+   console.log(totalDiscount);
+
+
+
+
+
 
 
  }
