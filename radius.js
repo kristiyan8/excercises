f21106258dbecbed4767ca5e3a569ad33beb72e2
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-27 16:09:33 +0200

    54
    46

---
+++ radius.js
@@ -0,0 +1,10 @@
+function radianstoDegree(input) {
+    let radians = Number(input[0]);
+    let degrees = radians * 180/Math.PI;
+
+    console.log(degrees);
+    
+
+
+}
+radianstoDegree(["3.1416"]);
