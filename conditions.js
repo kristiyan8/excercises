fa2c72f8fd1103263ae9d5a941167e64dbfd8b1c
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-29 20:55:25 +0200

    234
    24

--- conditions.js
+++ conditions.js
@@ -1,12 +1,17 @@
 function conditions() {
-    let number = 5;
+    let number = 15;
 
     if(number < 10){
         console.log('Number is smaller than 10');
          
 
+        
     }
+    else{
+        console.log('Number is bigger than or equal of  10');
 
+    }
+    
 
 }
 conditions();
