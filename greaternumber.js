826aa63a185dc399a05517d49f8c36b93af0ebf6
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-01-31 16:07:49 +0200

    234
    234

---
+++ greaternumber.js
@@ -0,0 +1,13 @@
+function opit(input) {
+    let firstNumber = Number(input[0]);
+    let secondNumber = Number(input[1]);
+
+    if(firstNumber > secondNumber){
+        console.log(firstNumber);
+    } else{
+        console.log(secondNumber);
+    }
+
+
+}
+opit(["5" , "8"]);
