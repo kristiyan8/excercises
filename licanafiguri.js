f6b14d5155ea6d5b55ceaf005ccf13e4bbe756c4
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-02 15:40:06 +0200

    23413213
    23413

--- licanafiguri.js
+++ licanafiguri.js
@@ -17,9 +17,16 @@ function demo() {
     
 
     } else if (figureName === 'circle') {
+        let r = Number(input[1]);
+        let area = Math.PI * Math.pow(r,2);
+        console.log(area.toFixed(3));
 
   
     } else if (figureName === 'triangle') {
+        let a = Number(input[1]);
+        let ha = Number(input[2]);
+        let area = (a * ha) / 2;
+        console.log(area.toFixed(3));
 
     }
 
