bd1847060704b44ad7dcd82ceefb01cb957951ae
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-02 15:29:36 +0200

    43242
    244

---
+++ licanafiguri.js
@@ -0,0 +1,24 @@
+function demo() {
+    
+    let figureName = input[0];
+
+    if(figureName === 'square'){
+        let a = Number(input[1]);
+        let area = a * a;
+        console.log(area.toFixed(3));
+
+
+    } else if(figureName === 'rectangle') {
+
+    } else if(figureName === 'circle') {
+  
+    } else if(figureName === 'triangle') {
+
+    }
+
+
+
+    
+
+}
+demo(["square", "5"]);
