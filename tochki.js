f6865fd927de15ac4c0fa5faa585546c8dfa1de4
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-05 15:35:55 +0200

    12313
    1313

--- tochki.js
+++ tochki.js
@@ -1,4 +1,19 @@
-function broiTochki() {
+function broiTochki(input) {
+    let initialPoints = Number(input[0]);
+    let bonus = 0;
+    if (initialPoints <= 100) {
+        bonus = 5;
+
+    } else if (initialPoints <= 1000) {
+        bonus = 0.2 * initialPoints;
+    } else {
+        bonus = 0.1 * initialPoints;
+    }
+
+    if (initialPoints % 2 === 0) {
+        bonus += 1;
+    }
+
 
 }
 broiTochki(["20"]);
