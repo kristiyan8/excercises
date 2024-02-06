3d749f65b359e3b32e87eef4f76f375eb3e6bd79
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-06 21:09:13 +0200

    234
    2424

---
+++ obedna.js
@@ -0,0 +1,11 @@
+function obednaSerial(input) {
+    let serialName = Number(input[0]);
+    let episodeTime = Number(input[1]);
+    let breakTime = Number(input[2]);
+
+    let LunchTime = 
+
+}
+obednaSerial(["Game of Thrones",
+"60",
+"96"]);
