b64b6eeac0e381b11cc1b7ab5ae19d59d9e15ce4
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-06 16:13:03 +0200

    24
    2434

---
+++ godzilavskong.js
@@ -0,0 +1,13 @@
+function godzilavskong(input) {
+    let decor = 0.10 * budget;
+    let budget = Number(input[0]);
+    let statistiBroi = Number(input[1]);
+    let oblekloPrice = Number(input[2]);
+
+    
+
+
+}
+godzilavskong(["20000",
+"120",
+"55.5"]);
