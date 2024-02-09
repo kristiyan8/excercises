f9fa08c5d98aa675ba550a2c94be735b9fb93f75
Кристиян Кирилов <kristiyan85292@gmail.com>
2024-02-09 19:15:51 +0200

    43242
    424324

--- sumiranenasec.js
+++ sumiranenasec.js
@@ -1,7 +1,12 @@
 function sumirane(input) {
     let time1 = Number(input[0]);
     let time2 = Number(input[1]);  
-    let time3 = Number(input[2]);     
+    let time3 = Number(input[2]);   
+    
+    let totalTime = time1 + time2+ time3;
+
+    let minutes = totalTime / 60;
+
 
 }
 sumirane(["35",
