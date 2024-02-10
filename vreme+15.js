20136999f583fedaa850256f15c9201c24c66f0c
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-10 16:25:12 +0200

    123131
    1312

---
+++ vreme+15.js
@@ -0,0 +1,22 @@
+function vreme(input) {
+    let hour = Number(input[0]);
+    let minute = Number(input[1]);
+
+    let mins = hour * 60 + minute + 15;
+    
+    let newHour = Math.floor(mins / 60);
+    let newMin = mins % 60;
+    if (newHour == 24) {
+        newHour = 0;
+    }
+
+    if (newMin < 10) {
+        console.log(`${newHour}:0${newMin}`);
+
+    } else {
+        console.log(`${newHour}:${newMin}`);
+    }
+
+
+}
+vreme(["24" , "46"]);
