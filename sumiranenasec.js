9dad256e4db25127be5a135df3fbc5650cdeaddb
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-10 16:15:53 +0200

    342
    2342424

--- sumiranenasec.js
+++ sumiranenasec.js
@@ -5,7 +5,20 @@ function sumirane(input) {
     
     let totalTime = time1 + time2+ time3;
 
-    let minutes = totalTime / 60;
+    let minutes = Math.floor(totalTime / 60);
+    let seconds = totalTime % 60;
+
+    if (seconds < 10) {
+        console.log(`${minutes}:0${seconds}`);
+    }else{
+        console.log(`${minutes}:${seconds}`);
+
+    }
+
+    
+      
+
+
 
 
 }
