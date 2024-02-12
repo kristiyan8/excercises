a7b6dc83ab735a30b742333a00d1a4e7439bd386
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-12 16:08:19 +0200

    123
    1313

---
+++ switch.js
@@ -0,0 +1,14 @@
+function demo() {
+    let name = "Kris";
+
+    switch(name){
+       case 'asq':console.log('hi,jeno');break;
+       case 'ivo': console.log('sup');break;
+       case 'mitko': console.log('supp');break;
+       case 'Kris': console.log('W');break;
+    
+
+    }
+
+}
+demo();
