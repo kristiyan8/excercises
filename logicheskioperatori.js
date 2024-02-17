8f95e0a41f09e8b36ef53918e007b4a6750c8c4f
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-17 16:13:39 +0200

    234
    32424

---
+++ logicheskioperatori.js
@@ -0,0 +1,37 @@
+function demo(){
+    let number = 6;
+
+    if(number > 5){
+        if(number < 10){
+            if(number % 2 === 0){
+                console.log("correct number");
+            }
+        }
+    }
+    
+    if(number > 5 && number < 10 && number % 2 === 0){
+        console.log('correct number');
+    }
+
+
+
+}
+demo();
+
+function demo1() {
+    let word = 'Demo';
+
+    if (word === 'Demo'){
+
+    }else if( word === 'example'){
+
+    }
+    
+    if( word === 'Demo' || word === 'example' ){
+
+    }
+  
+
+    
+}
+demo1();
