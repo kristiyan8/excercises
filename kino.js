da7a9232163613d18dafde58f0a4e9053801050c
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2024-02-21 21:19:21 +0200

    1313
    132123

--- kino.js
+++ kino.js
@@ -1,9 +1,22 @@
 function kino(input) {
     
     
-    let film = input[0];
+    let filmType = input[0];
     let rows = Number(input[1]);    
-    let colums = Number(input[2]);    
+    let colums = Number(input[2]);
+    
+    let totalPlaces = rows * colums;
+    let profit = 0;
+
+    if (filmType === 'Premiere') {
+         profit = totalPlaces * 12;
+    } else if (filmType = 'Normal') {
+        profit = totalPlaces * 7.5; 
+    } else if (filmType === 'Discount') {
+        profit = totalPlaces * 5;
+    }
+
+    console.log(`${profit.toFixed(2)} leva`);
 
 }
 kino(["Premiere",
